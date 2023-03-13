be242d8eff699ab82af1b92b7acee711309eee04
Flavio <flaviofm99@gmail.com>
2023-03-13 18:14:03 +0100

    M1L1

---
+++ L1/src/Test/ES3.java
@@ -0,0 +1,44 @@
+package Test;
+
+import java.util.Arrays;
+
+public class ES3 {
+	
+	public static void main(String args[]) {
+		String arr[] = {"a", "b", "c", "d", "e"};
+		
+		String[] na = pushTerzo(arr, "F");
+		
+		System.out.println(na.toString());
+//		for(int i = 0; i < na.length; i++) {
+//			System.out.print(na[i] + ", ");
+//		}
+
+		System.out.println(Arrays.toString(na));
+		
+		
+	}
+	
+	public static String[] pushTerzo(String[] a, String insert) {
+		String[] newArr = new String[a.length + 1];
+		for(int i = 0; i < newArr.length; i++) {
+			if(i < 2) {
+				newArr[i] = a[i];
+			} else if(i == 2) {
+				newArr[i] = insert;
+			} else {				
+				newArr[i] = a[i-1];
+			}
+			/*
+			 * push
+			if(i < newArr.length-1) {
+				newArr[i] = a[i];
+			} else {
+				newArr[i] = insert;
+			}*/
+		}
+		
+		return newArr;
+	}
+	
+}
